aaba8d424587b5bc793f83b240d8445996407b05
Giantory <giantory.bryant@gmail.com>
2022-05-14 21:12:04 -0500

    feat(react-zm): add Home components

---
+++ packages/react-zm/src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import './styles.sass'
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <button className="navbar-button">Crear una cuenta</button>
+        </div> 
+    )
+}
+
+export default Navbar
